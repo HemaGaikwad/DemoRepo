b9826424da58cd7170ae5e22c979498c121d820c
hema <hema.gowda1991@gmail.com>
2019-07-18 12:20:27 +0530

    Version 1.4

---
+++ Sample.java
@@ -0,0 +1,22 @@
+public class Sample
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello! Welcome to java programming");
+	}
+}
+
+
+/*
+public - access specifier which specifies the accessibility/ availability of a class or method or a variable. this class is available throughout the projecr
+class - keyword whcih is used to create a block/entity which hold the attributes and behavior of an object
+Sample - identifier that is used to identify the block of code- class or method 
+static  - keyword. this belongs to a class rather than an object. this doesnot require object to access it.
+void - means nothing. keyword - return type of a method.
+main - identifier - name given to a method
+String args[] - parameters. Strings in java are classes. array of string objects as argument. Used to accept values from command line.
+System - inbuilt class in java.lang package(default package) input and output statements, set the classpath
+out - reference object of type System. responsible for output statements
+println - method used to print statements
+{} - specify the start and end of block of code 
+*/
